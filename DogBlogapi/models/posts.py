06976cb94ede31b9a1b5017293b35f73cee22837
TrentSisson <trent.c.sisson0@gmail.com>
2021-05-21 01:36:05 -0500

    created models and fixtures and views.

---
+++ DogBlogapi/models/posts.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db import models
+from.post_user import PostUser
+
+
+class Post(models.Model):
+    user = models.ForeignKey("PostUser", on_delete=models.CASCADE)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=2500)
