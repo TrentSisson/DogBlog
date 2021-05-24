59e1fcd6d314575b57f35642ead692bcda8323b3
TrentSisson <trent.c.sisson0@gmail.com>
2021-05-23 23:22:50 -0500

    saving progress

--- DogBlogapi/models/posts.py
+++ DogBlogapi/models/posts.py
@@ -1,8 +1,9 @@
 from django.db import models
+from .post_user import PostUser
 
 
 class Post(models.Model):
-    user = models.ForeignKey("PostUser", on_delete=models.CASCADE)
+    user = models.ForeignKey(PostUser, on_delete=models.CASCADE)
     date = models.DateField(auto_now=False, auto_now_add=False)
     title = models.CharField(max_length=50)
     text = models.CharField(max_length=2500)
