cc4ac042b9946b638cd37a36b4ddd5a56d61c58a
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-22 01:58:22 -0500

    updated setting.py

--- DogBlog/settings.py
+++ DogBlog/settings.py
@@ -104,3 +104,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
