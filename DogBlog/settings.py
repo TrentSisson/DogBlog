7abaa0d3999494c7afbf970a49ee52fe3805f2fe
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-22 23:23:49 -0500

    test

--- DogBlog/settings.py
+++ DogBlog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import os.path
+from django.conf import settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
