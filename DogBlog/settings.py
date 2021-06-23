97d77e57316c1e0b1d761a58df6fe8ca94865611
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-22 23:38:09 -0500

    test

--- DogBlog/settings.py
+++ DogBlog/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import os.path
+import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
