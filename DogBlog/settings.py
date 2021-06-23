6b1c6110598797bcca23e456ed172893380ac49f
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-22 23:43:23 -0500

    test

--- DogBlog/settings.py
+++ DogBlog/settings.py
@@ -10,13 +10,12 @@ from pathlib import Path
 import os
 import os.path
 import django_on_heroku
-import SECRET_KEY
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv(SECRET_KEY)
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = []
