ba5ea58e5180996ac7301d2e587e6f81ede00317
TrentSisson <trent.c.sisson0@gmail.com>
2021-06-23 18:56:04 -0500

    deploymentTesting

--- DogBlog/settings.py
+++ DogBlog/settings.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 from pathlib import Path
 import os
-import os.path
 import django_on_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
